0428ed2c2bb32df3c962f30f686d1e5beb956a97
leopold-gauthier <leopold.gauthier-de-porry@laplateforme.io>
2023-02-09 10:51:07 +0100

    nav modified english

--- index.php
+++ index.php
@@ -19,19 +19,19 @@ require "./php/include/config.php"
         <nav>
             <?php
             if (isset($_SESSION['login'])) {
-                echo '<a href="./index.php">Accueil</a>';
+                echo '<a href="./index.php">Home</a>';
                 echo '<a href="./php/profil.php">Profil</a>';
                 echo '<a href="./php/planning.php">Planning</a>';
-                echo '<a href="./php/reservation-form.php">Réservation</a>';
+                echo '<a href="./php/reservation-form.php">Reservation</a>';
 
                 if ($_SESSION['login'] == 'admin') {
                     echo '<a href="./php/admin.php">Admin</a>';
                 }
                 echo '<a href="./php/logout.php"><i class="fa-solid fa-right-from-bracket"></i></a>';
             } else {
-                echo '<a href="./index.php">Accueil</a>';
-                echo '<a href="./php/connexion.php">Se connecter</a>';
-                echo "<a href='./php/inscription.php'>S'inscrire</a>";
+                echo '<a href="./index.php">Home</a>';
+                echo '<a href="./php/connexion.php">Login</a>';
+                echo "<a href='./php/inscription.php'>Sign Up</a>";
                 echo "<a href='./php/planning.php'>Planning</a>";
             }
             ?>
